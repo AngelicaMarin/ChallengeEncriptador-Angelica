274dbc94a8605f5c131b03912f17116e4ea2bad8
Angelica Marín <marinange22@gmail.com>
2024-08-26 21:03:19 -0600

    Commit Challenge

---
+++ js/encriptador.js
@@ -0,0 +1,125 @@
+/*------------Seleccion de elementos------------*/
+const btnEncriptar = document.querySelector(".btn-encrip");
+const txtEncriptar = document.querySelector(".pEncrip");
+const aviso = document.querySelector(".txt-alerta");
+const respuesta = document.querySelector(".evaluar");
+const contenido = document.querySelector(".mensaje-cont");
+const btnCopiar = document.querySelector(".btn-copy");
+const btnDesencriptar = document.querySelector(".btn-desencrip");
+
+/*------------Boton de encriptar------------*/
+btnEncriptar.addEventListener("click", e=>{
+    e.preventDefault();
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, '');
+    
+    if(texto == ""){
+        aviso.style.background = "#0a3871";
+        aviso.style.color = "#ffff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El campo de texto no debe estar vacio";
+
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== txt){
+        aviso.style.background = "#0a3871";
+        aviso.style.color = "#ffff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "No debe tener acentos ni caracteres especiales";
+
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== texto.toLowerCase()){
+        aviso.style.background = "#0a3871";
+        aviso.style.color = "#ffff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El texto debe ser todo en minuscula";
+
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else{
+        texto = texto.replace(/e/mg, "enter");
+        texto = texto.replace(/i/mg, "imes");
+        texto = texto.replace(/a/mg, "ai");
+        texto = texto.replace(/o/mg, "ober");
+        texto = texto.replace(/u/mg, "ufat");
+
+        respuesta.innerHTML = texto;
+        btnCopiar.style.visibility = "inherit";
+        contenido.remove();
+        
+    }
+
+    console.log(texto);
+})
+
+/*------------Boton de desencriptar------------*/
+btnDesencriptar.addEventListener("click", e=>{
+    e.preventDefault();
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, '');
+
+    if(texto == ""){
+        aviso.style.background = "#0a3871";
+        aviso.style.color = "#ffff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El campo de texto no debe estar vacio";
+
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== txt){
+        aviso.style.background = "#0a3871";
+        aviso.style.color = "#ffff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "No debe tener acentos ni caracteres especiales";
+
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== texto.toLowerCase()){
+        aviso.style.background = "#0a3871";
+        aviso.style.color = "#ffff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El texto debe ser todo en minuscula";
+
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else{
+        texto = texto.replace(/enter/mg, "e");
+        texto = texto.replace(/imes/mg, "i");
+        texto = texto.replace(/ai/mg, "a");
+        texto = texto.replace(/ober/mg, "o");
+        texto = texto.replace(/ufat/mg, "u");
+
+        respuesta.innerHTML = texto;
+        btnCopiar.style.visibility = "inherit";
+        contenido.remove();
+        
+    }
+
+});
+
+/*------------Boton de copiar------------*/
+btnCopiar.addEventListener("click", e=>{
+    e.preventDefault();
+    let copiar = respuesta;
+    copiar.select();
+    document.execCommand("copy");
+});
